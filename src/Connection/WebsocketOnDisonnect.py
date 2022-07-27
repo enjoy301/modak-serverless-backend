11a9782f993cdf07e5fa38b2a9a0ea2485e3f825
enjoy301 <eunjong147@pusan.ac.kr>
2022-07-27 16:58:28 +0900

    feat: websocket connection lambda function 작성

---
+++ src/Connection/WebsocketOnDisonnect.py
@@ -0,0 +1,38 @@
+import os
+import pymysql
+
+
+def get_db_secrets():
+    secrets = {
+        'db': os.environ.get('db'),
+        'host': os.environ.get('host'),
+        'password': os.environ.get('password'),
+        'port': int(os.environ.get('port')),
+        'username': os.environ.get('username'),
+    }
+
+    return secrets
+
+
+def lambda_handler(event, context):
+    secrets = get_db_secrets()
+
+    connection_id = event['requestContext']['connectionId']
+
+    connection = pymysql.connect(
+        host=secrets['host'],
+        port=secrets['port'],
+        user=secrets['username'],
+        passwd=secrets['password'],
+        db=secrets['db']
+    )
+    cursor = connection.cursor()
+
+    cursor.execute(f"update user_temp set connection_id=NULL where connection_id='{connection_id}'")
+
+    connection.commit()
+    connection.close()
+
+    return {
+        'statusCode': 200
+    }
