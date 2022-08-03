4c7555d98e60f16818f2a577c14fc26b20aa76ac
enjoy301 <eunjong147@pusan.ac.kr>
2022-08-03 21:20:10 +0900

    feat: modify presigned url for upload policy

--- src/Media/GenerateUploadURL.py
+++ src/Media/GenerateUploadURL.py
@@ -4,18 +4,15 @@ from botocore.client import Config
 
 
 def lambda_handler(event, context):
-    user_id = event['queryStringParameters']['u']
-    family_id = event['queryStringParameters']['f']
-
     s3 = boto3.client('s3', config=Config(signature_version='s3v4'))
 
-    key = str(family_id) + "/" + str(user_id) + "/" + "${filename}"
     url = s3.generate_presigned_post(
         Bucket="chatapp-private",
-        Key=key,
+        Key="${filename}",
         Conditions=[
-            ["starts-with", "$x-amz-meta-is_first", ""],
-            ["starts-with", "$x-amz-meta-count", ""]
+            ["starts-with", "$x-amz-meta-user_id", ""],
+            ["starts-with", "$x-amz-meta-family_id", ""],
+            ["starts-with", "$x-amz-meta-image_count", ""]
         ],
         ExpiresIn=86400
     )
